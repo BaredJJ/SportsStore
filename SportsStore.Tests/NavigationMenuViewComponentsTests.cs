32536bc77f88196a44514b2b6909f4a9e06d0e79
BaredJJ <33960321+BaredJJ@users.noreply.github.com>
2018-12-28 21:31:26 +0300

    Добавлена простая корзина

---
+++ SportsStore.Tests/NavigationMenuViewComponentsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
+using SportsStore.Components;
+using SportsStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentsTests
+    {
+        [Fact]
+        public void Invoke_ValidArgument_CanSelectCategories()
+        {
+            var target = GetNavigationMenuComponents();
+            var categories = GetProducts().Select(x => x.Category).Distinct().ToArray();
+
+            var result =((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            Assert.True(Enumerable.SequenceEqual(categories, result));
+        }
+
+        [Theory]
+        [InlineData("Cat1")]
+        [InlineData("Cat2")]
+        [InlineData("Cat3")]
+        public void Invoke_ValidArgument_IndicatesSelectedCategory(string categoryName)
+        {
+            var target = GetNavigationMenuComponents();
+            target.ViewComponentContext = GetViewComponentContext();
+            target.RouteData.Values["category"] = categoryName;
+
+            var result =(string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            Assert.Equal(categoryName, result);
+        }
+
+
+        private ViewComponentContext GetViewComponentContext() => new ViewComponentContext
+        {
+            ViewContext = new ViewContext
+            {
+                RouteData = new RouteData()
+            }
+        };
+
+        private IQueryable<Product> GetProducts() => GetterFakeRepository.GetProductWithCategory();
+
+        private IProductRepository GetProductRepository() => GetterFakeRepository.GetProductRepository(GetterFakeRepository.GetProductWithCategory());
+        private NavigationMenuViewComponent GetNavigationMenuComponents() => new NavigationMenuViewComponent(GetProductRepository());
+    }
+}
---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Linq;
+using SportsStore.Infrastructure;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class CartController:Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CartController(IProductRepository productRepository) => 
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+
+        public ViewResult Index(string returnUrl) => View(new CartIndexViewModel
+        {
+            Cart = GetCart(),
+            ReturnUrl = returnUrl
+        });
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            var product = GetProduct(productId);
+            if(product != null)
+            {
+                var cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = GetProduct(productId);
+            if(product != null)
+            {
+                var cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Product GetProduct(int productId) => _productRepository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+        private Cart GetCart() => HttpContext.Session.GetJSon<Cart>("Cart") ?? new Cart();
+
+        private void SaveCart(Cart cart) => HttpContext.Session.SetJSon("Cart", cart);
+    }
+}
